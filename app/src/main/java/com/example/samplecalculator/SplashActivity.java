0f8b5d316a5ead1fd8b6076af01d3b35a7151e84
Justin Dearden <deardenj@uwindsor.ca>
2020-04-24 23:28:15 -0400

    Added Settings and Splash Screen

---
+++ app/src/main/java/com/example/samplecalculator/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.samplecalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+
+    /*
+     * Displays the splash screen for 3 seconds
+     * Loads in the screen as an intent then transitions to the main activity
+     * */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
